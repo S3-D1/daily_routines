242e54b14222f52c272cad14a2db55f95168b05e
S3-D1 <kegeldiddi@outlook.de>
2023-05-14 14:55:43 +0200

    add testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('daily_routines app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Daily Routines by S3-D1');
   });
 });
